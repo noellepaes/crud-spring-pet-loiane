18c1016e2a1370b2aa1a33560a0ad766199bb7d7
noellepaes <noellepaes12345@gmail.com>
2024-08-05 13:37:29 -0300

    inicio

---
+++ src/main/java/com/noelle/crud_spring_pet_loiane/CrudSpringPetLoianeApplication.java
@@ -0,0 +1,13 @@
+package com.noelle.crud_spring_pet_loiane;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudSpringPetLoianeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudSpringPetLoianeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/noelle/crud_spring_pet_loiane/CrudSpringPetLoianeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noelle.crud_spring_pet_loiane;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudSpringPetLoianeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
