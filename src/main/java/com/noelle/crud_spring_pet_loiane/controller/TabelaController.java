cc05402a11a397591589f13dcc8ec8c29a9e231b
“noellepaes” <noellepaes12345@gmail.com>
2024-10-21 23:51:12 -0300

    sei la o que com id

--- src/main/java/com/noelle/crud_spring_pet_loiane/controller/TabelaController.java
+++ src/main/java/com/noelle/crud_spring_pet_loiane/controller/TabelaController.java
@@ -7,6 +7,7 @@ import javax.swing.text.html.parser.Entity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +19,8 @@ import com.noelle.crud_spring_pet_loiane.model.Tabela;
 import com.noelle.crud_spring_pet_loiane.repository.TabelaRepository;
 
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 @RequestMapping("api/tabela")
@@ -31,6 +34,14 @@ public class TabelaController {
         return tabelaRepository.findAll();
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<Tabela> findbyId(@PathVariable Long id) {
+        return tabelaRepository.findById(id)
+        .map(record -> ResponseEntity.ok().body(record))
+        .orElse(ResponseEntity.notFound().build());
+    }
+    
+
     @PostMapping
     @ResponseStatus(code = HttpStatus.CREATED)
     public Tabela create(@RequestBody Tabela tabela){
@@ -39,13 +50,5 @@ public class TabelaController {
         // return ResponseEntity.status(HttpStatus.CREATED)
         //     .body(tabelaRepository.save(tabela));
     }
-    // @PostMapping
-    // public ResponseEntity<Tabela> create(@RequestBody Tabela tabela) {
-    //     try {
-    //         Tabela savedTabela = tabelaRepository.save(tabela);
-    //         return new ResponseEntity<>(savedTabela, HttpStatus.CREATED);
-    //     } catch (Exception e) {
-    //         return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
-    //     }
-    // }
+
 }
